b8867c4bdfc83244e7aeb1b1951494520f1a16f2
Dhanush Shetty <133327430+Dhanus45@users.noreply.github.com>
2024-10-26 17:00:54 +0530

    Add files via upload

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,19 @@
-import { useState } from 'react'
-
-export default function Search(){
-    Const[querry,Setquerry]=useState("")
+import { useEffect, useState } from 'react'
+const URL="https://api.spoonacular.com/recipes/complexSearch"
+const API="e32e5439ce714f91852efdc6ae15cc0f"
+export default function Search({foodData,setfoodData}){
+    const[querry,Setquerry]=useState("pizza")
+    useEffect(()=>{
+       async function FetchFood(){
+          const resposne= await fetch(`${URL}?query=${querry}&apiKey=${API}`)
+          const data= await resposne.json()
+          console.log(data.results)
+          setfoodData(data.results)
+        }
+    },[querry])
     return <div>
-        <input type="text" value={querry} onChange={(e)=>Setquerry(e.target.value)} />
+        <input type="text" 
+        value={querry} 
+        onChange={(e)=>Setquerry(e.target.value)}/>
     </div>
 }
